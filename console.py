63e6c0e6aff575b922a6d9793c86e6fd2da818c7
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-03-26 23:47:59 +0000

    console implementation

--- console.py
+++ console.py
@@ -2,17 +2,23 @@
 
 import cmd, sys
 
-class AirBnB(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     '''console execution'''
     intro = 'welcome to the AirBnB console make by Coulby and Akinla. \n'
-    prompt = '(AirBnB)'
+    prompt = '(hbnb)'
     
     '''Methods to close console'''
     def do_exit(self, arg):
-        'Stop recording, close the AirBnB console, and exit:  EXIT '
+        'Stop recording, close the AirBnB console, and exit: quit '
         print('Thank you for using AirBnB console make by Coulby and Akinla')
         self.close()
-        exit()
+        quit()
         return True
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
+            
 if __name__ == '__main__':
-    AirBnB().cmdloop()
+    HBNBCommand().cmdloop()
