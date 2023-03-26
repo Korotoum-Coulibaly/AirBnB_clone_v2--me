9292d8b224e6e814fc7d72a01e40c175edd58104
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-03-26 23:39:54 +0000

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
 
-class BaseModel():
-    '''Contain all methods basics'''
-    '''variable self is call to save information in FileStorage'''
-    
-    def save(self):
-       self.updated_at = datetime.now()
-        '''FileStorage instanciation'''
-        storage = FileStorage()
-        '''save data to filestorage'''
-        storage.save()
+import uuid
+from datetime import datetime
+from models import storage
 
+
+class BaseModel:
+
+    """Class for base model of object hierarchy."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of a Base instance.
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -19,6 +43,12 @@ class BaseModel():
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
@@ -28,4 +58,3 @@ class BaseModel():
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
-
