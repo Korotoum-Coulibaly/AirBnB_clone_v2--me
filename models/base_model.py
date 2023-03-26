425dc50bada0ca11c8ac574f3e7ae1bb8b320162
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-03-26 23:35:46 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,27 @@ class BaseModel():
     '''variable self is call to save information in FileStorage'''
     
     def save(self):
-        id = ""
-        update_at = ""
-        created_at = ""
-
+       self.updated_at = datetime.now()
         '''FileStorage instanciation'''
         storage = FileStorage()
         '''save data to filestorage'''
         storage.save()
+
+
+    def __str__(self):
+        """Returns a human-readable string representation
+        of an instance."""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+
+    def to_dict(self):
+        """Returns a dictionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
 
