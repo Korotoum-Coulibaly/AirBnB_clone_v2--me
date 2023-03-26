02dd49220460efdfd08dd2b95526eb3fed88337c
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-03-26 23:53:52 +0000

    doquit doeof dodefault and do emptyline task 6

--- console.py
+++ console.py
@@ -8,17 +8,39 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     
     '''Methods to close console'''
-    def do_exit(self, arg):
-        'Stop recording, close the AirBnB console, and exit: quit '
-        print('Thank you for using AirBnB console make by Coulby and Akinla')
-        self.close()
-        quit()
+   def do_quit(self, arg):
+        """Quit command to exit the program."""
         return True
-    def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
 
-            
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+     def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
