c287115eab33359172eb8774f3a64aecf8c92692
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2023-04-19 02:30:55 -0700

    console.py quit and EOF)

--- models/place.py
+++ models/place.py
@@ -8,9 +8,10 @@ class Place(BaseModel):
     name = ""
     city_id = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
